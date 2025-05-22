5d3e0e9f4ec2eddd59d9217a7741de2677b739e4
Lachlan Grose <lachlan.grose@gmail.com>
2025-05-23 08:37:15 +1000

    disable marching cubes intersection code

--- loop_cgal/bindings.cpp
+++ loop_cgal/bindings.cpp
@@ -16,18 +16,18 @@ PYBIND11_MODULE(loop_cgal, m)
           py::arg("num_steps"),
           py::arg("iso_value"),
           "Generate a mesh from a scalar field using Marching Cubes.");
-    m.def("compute_optimized_intersection", &compute_optimized_intersection,
-            py::arg("scalar_field1"),
-            py::arg("scalar_field2"),
-            py::arg("iso_value1"),
-            py::arg("iso_value2"),
-            py::arg("origin_x"),
-            py::arg("origin_y"),
-            py::arg("origin_z"),
-            py::arg("cell_size_x"),
-            py::arg("cell_size_y"),
-            py::arg("cell_size_z"),
-            "Compute the optimized intersection of two isosurfaces.");
+//     m.def("compute_optimized_intersection", &compute_optimized_intersection,
+//             py::arg("scalar_field1"),
+//             py::arg("scalar_field2"),
+//             py::arg("iso_value1"),
+//             py::arg("iso_value2"),
+//             py::arg("origin_x"),
+//             py::arg("origin_y"),
+//             py::arg("origin_z"),
+//             py::arg("cell_size_x"),
+//             py::arg("cell_size_y"),
+//             py::arg("cell_size_z"),
+//             "Compute the optimized intersection of two isosurfaces.");
             
 
 
