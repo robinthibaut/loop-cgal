13833bcd5f6a029705360d89ae7db61c4ddadc3b
Lachlan Grose <lachlan.grose@gmail.com>
2025-05-27 08:35:38 +1000

    adding pyvista surface clipping wrapper

--- loop_cgal/__init__.py
+++ loop_cgal/__init__.py
@@ -1,1 +1,31 @@
 from .loop_cgal import *
+import pyvista as pv
+import numpy as np
+def clip_pyvista_polydata(
+    surface_1: pv.PolyData,
+    surface_2: pv.PolyData,
+) -> pv.PolyData:
+    """
+    Clip two pyvista PolyData objects using the CGAL library.
+
+    Parameters
+    ----------
+    surface_1 : pyvista.PolyData
+        The first surface to be clipped.
+    surface_2 : pyvista.PolyData
+        The second surface to be used for clipping.
+
+    Returns
+    -------
+    pyvista.PolyData
+        The resulting clipped surface.
+    """
+    surface_1 = surface_1.triangulate()
+    surface_2 = surface_2.triangulate()
+    mesh = clip_surface(
+        surface_1.faces.reshape(-1, 4)[:, 1:].copy(),
+        np.array(surface_1.points).copy(),
+        surface_2.faces.reshape(-1, 4)[:, 1:].copy(),
+        np.array(surface_2.points).copy(),
+    )
+    return pv.PolyData.from_regular_faces(mesh.vertices, mesh.triangles)
